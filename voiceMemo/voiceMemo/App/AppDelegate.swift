5ae16a89dd9e48091da68cdc7114743cf699cae9
MaryJo-github <jotjdal0215@gmail.com>
2024-04-01 11:22:10 +0900

    ✨Feat: LaunchScreen 생성 및 프로젝트 설정

---
+++ voiceMemo/voiceMemo/App/AppDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  AppDelegate.swift
+//  voiceMemo
+//
+//  Created by MARY on 2024/04/01.
+//
+
+import Foundation
---
+++ voiceMemo/voiceMemo/App/voiceMemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  voiceMemoApp.swift
+//  voiceMemo
+//
+//  Created by MARY on 2024/04/01.
+//
+
+import SwiftUI
+
+@main
+struct voiceMemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
